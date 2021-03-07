7b26a0aab2502ac62ac16b3a83396359adbfbfe4
José Esmerino <jose.esmerino@gmail.com>
2021-03-06 22:01:18 -0300

    Fix factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :event do
-    data { "#{JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'events', 'issue.json')))}" }
+    data { JSON.parse(File.read(Rails.root.join('spec', 'fixtures', 'events', 'issue.json'))) }
   end
 end
