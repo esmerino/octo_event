ae0814aad4cbbde4105d33c4943729f402b6313b
José Esmerino <jose.esmerino@gmail.com>
2021-03-06 21:59:16 -0300

    Scope search by issue

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   validates :data, presence: true
+  scope :by_issue, ->(issue) { where("data ->'issue'->>'number' = ?", issue.to_s) }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Event, type: :model do
   describe "#validations" do
     it { expect(Event.create(data: nil)).to_not be_valid }
   end
+
+  describe "#scopes" do
+    context "#by_issue" do
+      let!(:event) { create(:event) }
+
+      it do
+        result = Event.by_issue(event.data['issue']['number'])
+        expect(result[0].data['issue']['number']).to eq(event.data['issue']['number'])
+      end
+    end
+  end
 end
